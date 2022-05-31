0d929ef55948979c313cbad3ac6bcb56d544e054
JINTO N SEBASTIAN <65418412+jinto8301@users.noreply.github.com>
2022-05-31 15:30:34 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -138,6 +138,8 @@ async def re_enable_chat(bot, message):
 
 @Client.on_message(filters.command('stats') & filters.incoming)
 async def get_ststs(bot, message):
+    await message.reply_sticker(
+            'CAACAgUAAxkBAAEBHMxileZuMfzTBQh4j-JhAvM1C5nH-QAC4QYAAiKJsFRZtKAPqtEofSQE')
     rju = await message.reply('Fetching stats..')
     total_users = await db.total_users_count()
     totl_chats = await db.total_chat_count()
@@ -146,7 +148,12 @@ async def get_ststs(bot, message):
     free = 536870912 - size
     size = get_size(size)
     free = get_size(free)
-    await rju.edit(script.STATUS_TXT.format(files, total_users, totl_chats, size, free))
+    rju2 = await rju.edit('▣▣▢▢▢▢')
+    rju3 = await rju2.edit('▣▣▣▢▢▢')
+    rju4 = await rju3.edit('▣▣▣▣▢▢')
+    rju5 = await rju4.edit('▣▣▣▣▣▢')
+    rju6 = await rju5.edit('▣▣▣▣▣▣')
+    await rju6.edit(script.STATUS_TXT.format(files, total_users, totl_chats, size, free))
 
 
 # a function for trespassing into others groups, Inspired by a Vazha
